f0dd0e8da38ce2694c97abdb6f968fcf4759b660
Matt Rosensweig <matthew.rosensweig@gmail.com>
2020-10-02 20:29:55 -0400

    Fully implement /types endpoint
    /pokemon endpoint still with only basic functionality.

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  
+  console.log('validate bearer token middleware');
+  
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
 
-app.use((req, res) => {
-  res.send('Hello, Express!');
+  next();
 });
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  res.json(POKEDEX);
+}
+
+app.get('/pokemon', handleGetPokemon);
 
 const PORT = 8000;
 
