0cec0049e55388fd6c18c3059d28e07be9dc2983
Matt Rosensweig <matthew.rosensweig@gmail.com>
2020-10-04 16:00:52 -0400

    Implement all functionality and update code for deployment

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const POKEDEX = require('./pokedex.json');
 
 const app = express();
-app.use(morgan('dev'));
+
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
+
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
-  
-  console.log('validate bearer token middleware');
   
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
@@ -17,6 +22,20 @@ app.use(function validateBearerToken(req, res, next) {
 
   next();
 });
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT)
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
@@ -27,13 +46,27 @@ function handleGetTypes(req, res) {
 app.get('/types', handleGetTypes);
 
 function handleGetPokemon(req, res) {
-  res.json(POKEDEX);
+  const { name = '', type = '' } = req.query;
+
+  if (type) {
+    if (!validTypes.includes(type)) {
+      return res.status(400).send('Type must be one of the provided types');
+    }
+  }
+
+  const results = POKEDEX.pokemon.filter(entry => (
+    entry.name.toLowerCase().includes(name.toLowerCase())
+  ));
+
+  if (type) {
+    const filtered = results.filter(entry => (
+      entry.type.includes(type)
+    ));
+    return res.json(filtered);
+  }
+  
+  return res.json(results);
 }
 
 app.get('/pokemon', handleGetPokemon);
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
